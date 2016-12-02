4296c0da63588a2f0736954d4a7119ba279c95f9
Brian Pritt <brianpritt@gmail.com>
2016-12-02 10:51:19 -0800

    add argument to ignore punctuation, without using regex

--- Objects/WordCount.cs
+++ Objects/WordCount.cs
@@ -17,17 +17,24 @@ namespace WordCount.Objects
     }
     public void CountRepeats()
     {
-      string[] arrayUserString = this.UserString.Split();
+      string[] arrayUserString = this.UserString.Split();// splits user input into array of words
       string checkWord = this.UserCheck.ToLower();
+      List<string> punctList = new List<string>() {"!","@","#","$","%","^","&","*",",",".",".","`"};
       int loopVar = 0;
-      foreach(string word in arrayUserString)
+      foreach(string word in arrayUserString)//loops through array and compares each word to check word
       {
+        foreach(string punct in punctList){
+          if (word.Contains(punct))
+          {
+            arrayUserString[loopVar] = word.Replace(punct,"");
+          }
+        }
         if (arrayUserString[loopVar].ToLower() == checkWord)
         {
-          this.Repeats +=1;
+          this.Repeats +=1;//if there is a match, this counter will go up
         }
         loopVar ++;
-    }
+      }
     }
   }
 }
