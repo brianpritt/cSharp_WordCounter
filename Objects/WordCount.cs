e4bf01a30177a30c6479f5841dee3747c1181fa7
Brian Pritt <brianpritt@gmail.com>
2016-12-02 09:27:45 -0800

    add test for spec 1, and pass

--- Objects/WordCount.cs
+++ Objects/WordCount.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 
 namespace WordCount.Objects
 {
-  public class Word
+  public class RepeatCounter
   {
     public string UserString {get; set;}
     public string UserCheck {get; set;}
+    public int Repeats {get; set;}
 
-    public Word(string userInputString, string userInputCheck)
+    public RepeatCounter(string userInputString, string userInputCheck)
     {
       this.UserString = userInputString;
       this.UserCheck = userInputCheck;
+      this.Repeats = 0;
     }
-    public int Counter()
+    public int CountRepeats()
     {
-      string 
+      if (this.UserString == this.UserCheck)
+      {
+        this.Repeats +=1;
+      }
+      int returnValue = this.Repeats;
+      
+      return returnValue;
     }
   }
 }
---
+++ Tests/WordCountTest.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using WordCount.Objects;
+
+namespace  WordCount
+{
+  public class WordCountTest
+  {
+    [Fact]
+    public void IsMatch_OneWord_true()
+    {
+      //Arrange
+      string word = "this";
+      string checkWord = "this";
+      RepeatCounter newCounter = new RepeatCounter(word, checkWord);
+      //Act
+      int theTest = newCounter.CountRepeats();
+      //Assert
+      Assert.Equal(true, theTest == 1);
+    }
+  }
+}
