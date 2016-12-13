0f05e2c0fd869a4ea1713d866d9af1a00860ccf5
Brian Pritt <brianpritt@gmail.com>
2016-12-12 22:57:07 -0800

    fix test wording in README, add a test that returns false in matching, and reorder tests from simplest to most complex.

--- Tests/WordCountTest.cs
+++ Tests/WordCountTest.cs
@@ -20,10 +20,34 @@ namespace  WordCount
       Assert.Equal(true, newCounter.Repeats == 1);
     }
     [Fact]
-    public void IsMatch_StringMatch_true()
+    public void IsMatch_OneWord_False()
     {
       //Arrange
-      string word = "this is nice";
+      string word = "this";
+      string checkWord = "that";
+      RepeatCounter newCounter = new RepeatCounter(word, checkWord);
+      //Act
+      newCounter.CountRepeats();
+      //Assert
+      Assert.Equal(false, newCounter.Repeats == 1);
+    }
+    [Fact]
+    public void IsMatch_StringMatchMultiples_true()
+    {
+      //Arrange
+      string word = "this this";
+      string checkWord = "this";
+      RepeatCounter newCounter = new RepeatCounter(word, checkWord);
+      //Act
+      newCounter.CountRepeats();
+      //Assert
+      Assert.Equal(true, newCounter.Repeats == 2);
+    }
+    [Fact]
+    public void IsMatch_StringMatchCase_true()
+    {
+      //Arrange
+      string word = "THIS";
       string checkWord = "this";
       RepeatCounter newCounter = new RepeatCounter(word, checkWord);
       //Act
@@ -32,10 +56,10 @@ namespace  WordCount
       Assert.Equal(true, newCounter.Repeats == 1);
     }
     [Fact]
-    public void IsMatch_StringMatchCase_true()
+    public void IsMatch_StringMatch_true()
     {
       //Arrange
-      string word = "THIS is nice";
+      string word = "this is nice";
       string checkWord = "this";
       RepeatCounter newCounter = new RepeatCounter(word, checkWord);
       //Act
@@ -54,18 +78,6 @@ namespace  WordCount
       newCounter.CountRepeats();
       //Assert
       Assert.Equal(true, newCounter.Repeats == 1);
-    }
-    [Fact]
-    public void IsMatch_StringMatchMultiples_true()
-    {
-      //Arrange
-      string word = "this here, this is nice";
-      string checkWord = "this";
-      RepeatCounter newCounter = new RepeatCounter(word, checkWord);
-      //Act
-      newCounter.CountRepeats();
-      //Assert
-      Assert.Equal(true, newCounter.Repeats == 2);
     }
   }
 }
